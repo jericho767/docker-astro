d90dba1cd60f135c28b05b42c8a6a3129d560eee
Jkoi <jericho.6367@gmail.com>
2022-02-16 16:23:13 +0800

    Fix NGINX service container.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# TODO UPDATE THIS!
+
 # Docker Setup for ASTRO Systems
 Docker setup to contain all(PMS, VMS, Crewing, Purchasing, Astro -- Main) ASTRO systems.  
 
@@ -8,18 +10,24 @@ cp .env.example .env
 ```
 Fill in variables
 ```
-MYSQL_ROOT_PASSWORD=p@ss1234!           # root password of the root mysql user
-MYSQL_DATABASE=YOUR_DATABASE_NAME       # database that will be created
-MYSQL_USER=YOUR_DATABASE_USER           # mysql user
-MYSQL_PASSWORD=YOUR_DATABASE_USER       # user password
+...
+...
+MYSQL_ROOT_PASSWORD=r00t!234 
+MYSQL_DATABASE=astro
+MYSQL_USER=username
+MYSQL_PASSWORD=p@ssw0rd!
+MAIL_NAME=mail.domain.jp
+MAIL_HOST=email-smtp.us-east-1.amazonaws.com
+MAIL_PORT=587
+MAIL_USERNAME=AKIAJG2ZHDOL5JJ7O23Q
+MAIL_PASSWORD=BAsPuqpqjA5dQTJAuNMsddGwANpOQdbgIoFFa5I74KyW
+DOCKER_IP=172.0.0.0
+TIMEZONE=Asia/Manila
 ....
 ....
-....
-API_DOMAIN=api.app.loc
-APP_DOMAIN=app.loc
 ```
-For local development, add the following lines to host file:
+Add the listed domains in the `.env` to the host file:
 ```
-192.168.99.100    app.loc api.app.loc
+192.168.99.100 api.app-astro.loc app-astro.loc api.app-pms.loc app-pms.loc api.app-vms.loc app-vms.loc api.app-crewing.loc app-crewing.loc api.app-purchasing.loc app-purchasing.loc
 ```
 Note: Replace `192.168.99.100` with your docker machine IP.
--- docker/nginx-validate/scripts/configure.sh
+++ docker/nginx-validate/scripts/configure.sh
@@ -1,31 +1,149 @@
 #!/bin/bash
 mkdir -p /etc/nginx/ssl
-sed -i "s/API_DOMAIN/$API_DOMAIN/g" /etc/nginx/conf.d/default.conf
-sed -i "s/APP_DOMAIN/$APP_DOMAIN/g" /etc/nginx/conf.d/default.conf
+# Replace the APP and API DOMAIN strings from default.conf file with the environment variable
+sed -i "s|ASTRO_API_DOMAIN|$ASTRO_API_DOMAIN|g" /etc/nginx/conf.d/default.conf
+sed -i "s|PMS_API_DOMAIN|$PMS_API_DOMAIN|g" /etc/nginx/conf.d/default.conf
+sed -i "s|VMS_API_DOMAIN|$VMS_API_DOMAIN|g" /etc/nginx/conf.d/default.conf
+sed -i "s|CREWING_API_DOMAIN|$CREWING_API_DOMAIN|g" /etc/nginx/conf.d/default.conf
+sed -i "s|PURCHASING_API_DOMAIN|$PURCHASING_API_DOMAIN|g" /etc/nginx/conf.d/default.conf
+sed -i "s|ASTRO_APP_DOMAIN|$ASTRO_APP_DOMAIN|g" /etc/nginx/conf.d/default.conf
+sed -i "s|PMS_APP_DOMAIN|$PMS_APP_DOMAIN|g" /etc/nginx/conf.d/default.conf
+sed -i "s|VMS_APP_DOMAIN|$VMS_APP_DOMAIN|g" /etc/nginx/conf.d/default.conf
+sed -i "s|CREWING_APP_DOMAIN|$CREWING_APP_DOMAIN|g" /etc/nginx/conf.d/default.conf
+sed -i "s|PURCHASING_APP_DOMAIN|$PURCHASING_APP_DOMAIN|g" /etc/nginx/conf.d/default.conf
+# Replace the BACKEND DESTINATION DIRECTORY strings from default.conf file with the environment variable
+sed -i "s|ASTRO_BE_DEST_DIR|$ASTRO_BE_DEST_DIR|g" /etc/nginx/conf.d/default.conf
+sed -i "s|PMS_BE_DEST_DIR|$PMS_BE_DEST_DIR|g" /etc/nginx/conf.d/default.conf
+sed -i "s|VMS_BE_DEST_DIR|$VMS_BE_DEST_DIR|g" /etc/nginx/conf.d/default.conf
+sed -i "s|CREWING_BE_DEST_DIR|$CREWING_BE_DEST_DIR|g" /etc/nginx/conf.d/default.conf
+sed -i "s|PURCHASING_BE_DEST_DIR|$PURCHASING_BE_DEST_DIR|g" /etc/nginx/conf.d/default.conf
+# Replace the FRONTEND DESTINATION DIRECTORY strings from default.conf file with the environment variable
+sed -i "s|ASTRO_FE_DEST_DIR|$ASTRO_FE_DEST_DIR|g" /etc/nginx/conf.d/default.conf
+sed -i "s|PMS_FE_DEST_DIR|$PMS_FE_DEST_DIR|g" /etc/nginx/conf.d/default.conf
+sed -i "s|VMS_FE_DEST_DIR|$VMS_FE_DEST_DIR|g" /etc/nginx/conf.d/default.conf
+sed -i "s|CREWING_FE_DEST_DIR|$CREWING_FE_DEST_DIR|g" /etc/nginx/conf.d/default.conf
+sed -i "s|PURCHASING_FE_DEST_DIR|$PURCHASING_FE_DEST_DIR|g" /etc/nginx/conf.d/default.conf
+
 openssl dhparam -out /etc/nginx/ssl/dhparam.pem 2048
 
 # Create Self-signed SSL Cert for APP Domain
-if [ ! -z "$APP_DOMAIN" ]
+if [ ! -z "$ASTRO_APP_DOMAIN" ]
 then
 openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
-  -keyout /etc/nginx/ssl/$APP_DOMAIN.key -out /etc/nginx/ssl/$APP_DOMAIN.cert -extensions san -config \
+  -keyout /etc/nginx/ssl/$ASTRO_APP_DOMAIN.key -out /etc/nginx/ssl/$ASTRO_APP_DOMAIN.cert -extensions san -config \
   <(echo "[req]";
     echo distinguished_name=req;
     echo "[san]";
-    echo subjectAltName=DNS:$APP_DOMAIN,IP:10.0.0.1
+    echo subjectAltName=DNS:$ASTRO_APP_DOMAIN,IP:10.0.0.1
     ) \
-  -subj /CN=$APP_DOMAIN
+  -subj /CN=$ASTRO_APP_DOMAIN
+fi
+
+if [ ! -z "$PMS_APP_DOMAIN" ]
+then
+openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
+  -keyout /etc/nginx/ssl/$PMS_APP_DOMAIN.key -out /etc/nginx/ssl/$PMS_APP_DOMAIN.cert -extensions san -config \
+  <(echo "[req]";
+    echo distinguished_name=req;
+    echo "[san]";
+    echo subjectAltName=DNS:$PMS_APP_DOMAIN,IP:10.0.0.1
+    ) \
+  -subj /CN=$PMS_APP_DOMAIN
+fi
+
+if [ ! -z "$VMS_APP_DOMAIN" ]
+then
+openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
+  -keyout /etc/nginx/ssl/$VMS_APP_DOMAIN.key -out /etc/nginx/ssl/$VMS_APP_DOMAIN.cert -extensions san -config \
+  <(echo "[req]";
+    echo distinguished_name=req;
+    echo "[san]";
+    echo subjectAltName=DNS:$VMS_APP_DOMAIN,IP:10.0.0.1
+    ) \
+  -subj /CN=$VMS_APP_DOMAIN
+fi
+
+if [ ! -z "$CREWING_APP_DOMAIN" ]
+then
+openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
+  -keyout /etc/nginx/ssl/$CREWING_APP_DOMAIN.key -out /etc/nginx/ssl/$CREWING_APP_DOMAIN.cert -extensions san -config \
+  <(echo "[req]";
+    echo distinguished_name=req;
+    echo "[san]";
+    echo subjectAltName=DNS:$CREWING_APP_DOMAIN,IP:10.0.0.1
+    ) \
+  -subj /CN=$CREWING_APP_DOMAIN
+fi
+
+if [ ! -z "$PURCHASING_APP_DOMAIN" ]
+then
+openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
+  -keyout /etc/nginx/ssl/$PURCHASING_APP_DOMAIN.key -out /etc/nginx/ssl/$PURCHASING_APP_DOMAIN.cert -extensions san -config \
+  <(echo "[req]";
+    echo distinguished_name=req;
+    echo "[san]";
+    echo subjectAltName=DNS:$PURCHASING_APP_DOMAIN,IP:10.0.0.1
+    ) \
+  -subj /CN=$PURCHASING_APP_DOMAIN
 fi
 
 # Create Self-signed SSL Cert for API Domain
-if [ ! -z "$API_DOMAIN" ]
+if [ ! -z "$ASTRO_API_DOMAIN" ]
 then
 openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
-  -keyout /etc/nginx/ssl/$API_DOMAIN.key -out /etc/nginx/ssl/$API_DOMAIN.cert -extensions san -config \
+  -keyout /etc/nginx/ssl/$ASTRO_API_DOMAIN.key -out /etc/nginx/ssl/$ASTRO_API_DOMAIN.cert -extensions san -config \
   <(echo "[req]";
     echo distinguished_name=req;
     echo "[san]";
-    echo subjectAltName=DNS:$API_DOMAIN,IP:10.0.0.1
+    echo subjectAltName=DNS:$ASTRO_API_DOMAIN,IP:10.0.0.1
     ) \
-  -subj /CN=$API_DOMAIN
+  -subj /CN=$ASTRO_API_DOMAIN
+fi
+
+if [ ! -z "$PMS_API_DOMAIN" ]
+then
+openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
+  -keyout /etc/nginx/ssl/$PMS_API_DOMAIN.key -out /etc/nginx/ssl/$PMS_API_DOMAIN.cert -extensions san -config \
+  <(echo "[req]";
+    echo distinguished_name=req;
+    echo "[san]";
+    echo subjectAltName=DNS:$PMS_API_DOMAIN,IP:10.0.0.1
+    ) \
+  -subj /CN=$PMS_API_DOMAIN
+fi
+
+if [ ! -z "$VMS_API_DOMAIN" ]
+then
+openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
+  -keyout /etc/nginx/ssl/$VMS_API_DOMAIN.key -out /etc/nginx/ssl/$VMS_API_DOMAIN.cert -extensions san -config \
+  <(echo "[req]";
+    echo distinguished_name=req;
+    echo "[san]";
+    echo subjectAltName=DNS:$VMS_API_DOMAIN,IP:10.0.0.1
+    ) \
+  -subj /CN=$VMS_API_DOMAIN
+fi
+
+if [ ! -z "$CREWING_API_DOMAIN" ]
+then
+openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
+  -keyout /etc/nginx/ssl/$CREWING_API_DOMAIN.key -out /etc/nginx/ssl/$CREWING_API_DOMAIN.cert -extensions san -config \
+  <(echo "[req]";
+    echo distinguished_name=req;
+    echo "[san]";
+    echo subjectAltName=DNS:$CREWING_API_DOMAIN,IP:10.0.0.1
+    ) \
+  -subj /CN=$CREWING_API_DOMAIN
+fi
+
+if [ ! -z "$PURCHASING_API_DOMAIN" ]
+then
+openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
+  -keyout /etc/nginx/ssl/$PURCHASING_API_DOMAIN.key -out /etc/nginx/ssl/$PURCHASING_API_DOMAIN.cert -extensions san -config \
+  <(echo "[req]";
+    echo distinguished_name=req;
+    echo "[san]";
+    echo subjectAltName=DNS:$PURCHASING_API_DOMAIN,IP:10.0.0.1
+    ) \
+  -subj /CN=$PURCHASING_API_DOMAIN
 fi
